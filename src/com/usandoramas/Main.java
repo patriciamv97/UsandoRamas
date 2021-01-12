0cfe6ae6c6c5418d0291f195ea8d69be38d66054
Patricia Maceiras <pmaceirasvarela@danielcastelao.org>
2021-01-12 10:59:38 +0100

    nueva rama bugFix

--- src/com/usandoramas/Main.java
+++ src/com/usandoramas/Main.java
@@ -3,6 +3,7 @@ package com.usandoramas;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Rama principal");
+        System.out.println("Salgo de la rama principal");
+        System.out.println("voy a la bugFix");
     }
 }
